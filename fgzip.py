a082331ded11bb9af89b59faf6761b18ef69c9b5
Marc-André Legault <legaultmarc@gmail.com>
2014-12-10 14:50:59 -0500

    Interfaces for generators and context managers.

--- fgzip.py
+++ fgzip.py
@@ -65,15 +65,20 @@ class GzipFileReader(object):
             return line
 
     def _reader_proc(self):
-        with gzip.open(self.fn) as f:
-            chunk = f.read(self.chunk_size)
-            while chunk:
-                self._q.put(chunk)
+        try:
+            with gzip.open(self.fn) as f:
                 chunk = f.read(self.chunk_size)
+                while chunk:
+                    self._q.put(chunk)
+                    chunk = f.read(self.chunk_size)
 
-        self._q.put(None)
-        self._q.close()
-        return
+            self._q.put(None)
+            self._q.close()
+            return
+
+        except KeyboardInterrupt:
+            self._q.close()
+            return
 
     def _parser(self):
         fragment = ""
@@ -102,6 +107,25 @@ class GzipFileReader(object):
         if fragment:
             yield fragment 
 
+    # Methods for the generator interface.
+    def next(self):
+        for i in self._iterator:
+            break
+
+    def __next__(self):
+        return self.next()
+
+    def __iter__(self):
+        return self._iterator
+
+    # Methods for the context manager interface.
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Kill the reader process.
+        if self._reader_proc.is_alive():
+            self._reader_proc.terminate()
 
 def read_gzip(fn):
     # Start a thread that reads the gzip file and pushes the data in a queue.
