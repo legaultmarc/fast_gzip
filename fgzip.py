48307661487eb8cc4a4493030b9ba394a3767da3
Marc-André Legault <legaultmarc@gmail.com>
2014-12-10 15:15:23 -0500

    Cleaned up

--- fgzip.py
+++ fgzip.py
@@ -5,46 +5,6 @@ import gzip
 import argparse
 from multiprocessing import Process, Queue
 
-def _reader(fn, queue, chunk_size=128 * 1000):
-    with gzip.open(fn) as f:
-        chunk = f.read(chunk_size)
-        while chunk:
-            queue.put(chunk)
-            chunk = f.read(chunk_size)
-
-    queue.put(None)
-    queue.close()
-    return
-
-
-def _parser(queue):
-    fragment = ""
-    chunk = queue.get()
-    while chunk is not None:
-        # Split in lines.
-        li = chunk.splitlines()
-
-        # If there was a previous fragment, we prepend the first line.
-        if fragment != "":
-            li[0] = fragment + li[0]
-            fragment = ""
-
-        # Maybe the chunk didn't end with a newline.
-        # We keep this (new) fragment.
-        if not chunk.endswith("\n"):
-            fragment = li[-1]
-            li = li[:-1]
-
-        for elem in li:
-            # Generate line by line
-            yield elem
-
-        chunk = queue.get()
-
-    if fragment:
-        yield fragment
-
-
 class GzipFileReader(object):
 
     def __init__(self, fn, chunk_size=128 * 1024):
@@ -127,18 +87,10 @@ class GzipFileReader(object):
         if self._reader_proc.is_alive():
             self._reader_proc.terminate()
 
-def read_gzip(fn):
-    # Start a thread that reads the gzip file and pushes the data in a queue.
-    q = Queue()
 
-    reader = Process(target=_reader, args=(fn, q))
-    reader.start()
-
-    # Count the lines in the file.
-    i = 0
-    for line in _parser(q):
-        i += 1
-    print i
-
-    reader.join()
+def open(fn, chunk_size=None):
+    if chunk_size is not None:
+        return GzipFileReader(fn, chunk_size)
+    else:
+        return GzipFileReader(fn)
 
--- settings.sample.py
+++
@@ -1,6 +0,0 @@
-# Rename this file to 'settings.py' if you have system specific
-# parameters to setup, e.g. if your zcat or gzip is not on the path or if you 
-# want to use a specific version of these utilities.
-
-GZIP_BIN='gzip'
-ZCAT_BIN='zcat'
